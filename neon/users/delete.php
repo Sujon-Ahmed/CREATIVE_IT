d093aa33c9971d24144d6073fe75236270b66881
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-01-13 00:44:31 +0600

    delete data with image form database

--- neon/users/delete.php
+++ neon/users/delete.php
@@ -4,6 +4,12 @@ require '../db.php';
 // check url get data
 if(isset($_GET['id'])) {
     $id = $_GET['id'];
+    // select query for get image
+    $select_image = "SELECT * FROM users WHERE id='$id'";
+    $select_image_result = mysqli_query($con, $select_image);
+    $after_assoc = mysqli_fetch_assoc($select_image_result);
+    $delete_from ="../uploads/users/".$after_assoc['profile_image'];
+    unlink($delete_from);
     // delete query
     $delete = "DELETE FROM `users` WHERE id = $id";
     $result = mysqli_query($con,$delete);
