8cb1809d0d482a064f5e89a2420b8ef717733a8e
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-01-13 12:11:42 +0600

    update image with password

--- neon/users/update.php
+++ neon/users/update.php
@@ -63,9 +63,43 @@ if(empty($name)) {
         $_SESSION['password_error'] = "At least 8 characters";
         header('location:edit_user.php');
     } else {
-        $update_user = "UPDATE users SET name='$name', email='$email', password='$after_hash' WHERE id='$id'";
-        $update_user_result = mysqli_query($con, $update_user);
-        header('location:show.php');
+        // update image with password felid
+        if($_FILES['profile_image']['name'] != '') {
+            $uploaded_file = $_FILES['profile_image'];
+            $after_explode = explode('.',$uploaded_file['name']);
+            $extension = end($after_explode);
+            $allowed_extension = array('jpg', 'jpeg', 'png');
+            if(in_array($extension, $allowed_extension)) {
+                // file size check
+                if($uploaded_file['size'] <= 2000000) {
+                    // delete old image
+                    $select_image = "SELECT * FROM users WHERE id=$id";
+                    $select_image_result = mysqli_query($con, $select_image);
+                    $after_assoc = mysqli_fetch_assoc($select_image_result);
+                    $delete_form = "../uploads/users/".$after_assoc['profile_image'];
+                    unlink($delete_form);
+                    // set new name with new location
+                    $file_name = $id.'.'.$extension;
+                    $new_location = "../uploads/users/".$file_name;
+                    move_uploaded_file($uploaded_file['tmp_name'], $new_location);
+                    // update all felid
+                    $update_user = "UPDATE users SET name='$name', email='$email', password='$after_hash', profile_image='$file_name' WHERE id='$id'";
+                    $update_user_result = mysqli_query($con, $update_user);
+                    header('location:show.php');
+                    
+                } else {
+                    $_SESSION['size_error'] = "The file is too large. Allowed maximum size is 2 MiB.";
+                header('location:edit_user.php');
+                }
+            } else {
+                $_SESSION['extension_error'] = "Invalid Extension";
+                header('location:edit_user.php');
+            }
+        } else {
+            $update_user = "UPDATE users SET name='$name', email='$email', password='$after_hash' WHERE id='$id'";
+            $update_user_result = mysqli_query($con, $update_user);
+            header('location:show.php');
+        }
     }
 }
 
