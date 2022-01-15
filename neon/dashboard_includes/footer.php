a98a362da818ec7fa2f014e5003b323aa18b63b9
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-01-15 23:21:00 +0600

    dashboard added

---
+++ neon/dashboard_includes/footer.php
@@ -0,0 +1,9 @@
+   <script src="/Creative-IT/neon/dashboard_assets/lib/jquery/jquery.js"></script>
+    <script src="/Creative-IT/neon/dashboard_assets/lib/popper.js/popper.js"></script>
+    <script src="/Creative-IT/neon/dashboard_assets/lib/bootstrap/bootstrap.js"></script>
+    <script src="/Creative-IT/neon/dashboard_assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+
+    <script src="/Creative-IT/neon/dashboard_assets/js/starlight.js"></script>
+
+  </body>
+</html>
