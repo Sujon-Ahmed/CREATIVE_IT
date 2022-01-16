12095fb0dc63b00030892cd2d7352f10696f47e4
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-01-16 13:26:30 +0600

    dashboard title change

--- neon/dashboard_includes/header.php
+++ neon/dashboard_includes/header.php
@@ -28,7 +28,7 @@
     <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
     <meta name="author" content="ThemePixels">
 
-    <title>Starlight Responsive Bootstrap 4 Admin Template</title>
+    <title>Neon Dashboard</title>
 
     <!-- vendor css -->
     <link href="/Creative-IT/neon/dashboard_assets/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
